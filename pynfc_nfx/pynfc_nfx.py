e4351b090f7980bd52c640c9c5197e1d08bdc8dc
timeyyy <timeyyy_da_man@hotmail.com>
2015-01-27 18:24:33 +0100

    's2'

--- pynfc_nfx/pynfc_nfx.py
+++ pynfc_nfx/pynfc_nfx.py
@@ -2,7 +2,9 @@
 import ctypes
 import os
 
-from . import blank_module		# By importing any sub module i can get the location of this module, required by ctypes.CDLL
+# By importing any sub module i can get the location of this module, required by ctypes.CDLL
+# Dunno why but it wasn't finding it after being installed
+from . import blank_module		
 def mod_path(mod):
 	return os.path.dirname(mod.__file__)
 PKG_PATH = mod_path(blank_module)
@@ -13,7 +15,6 @@ class pynfc_nfx():
 	'''
 Wrapper for some c code provided by the manufacturers
 	'''
-	
 	def __init__(self):
 		try:
 			self.test = ctypes.CDLL(os.path.join(BUILD_PATH, '_test.so'))
--- setup.py
+++ setup.py
@@ -5,35 +5,34 @@ import os
 from subprocess import call
 import shutil
 
-class build_nxppy(build):
+class build_pynfc_nfx(build):
 	def run(self):
 		starting_dir = os.getcwd()
-		src_dir = os.path.join(*(os.getcwd(),'pynfc_nfx', 'c_src','test'))
+		src_dir = os.path.join(*(os.getcwd(),'pynfc_nfx', 'c_src','NxpRdLib_PublicRelease'))
 		build_dir = os.path.join(os.getcwd(),'pynfc_nfx', 'build')
 		os.chdir(src_dir)
 
 		def compile():
-			call(["gcc -fpic test.c -O2 -c test.c"], shell=True )
-			call(["gcc -shared test.o -o _test.so"], shell=True )
+			call('cmake .', shell=True )
+            call('make', shell=True )
+			#~ call(["gcc -fpic test.c -O2 -c test.c"], shell=True )
+			#~ call(["gcc -shared test.o -o _test.so"], shell=True )
 			#~ print(os.listdir(src_dir))
-			shutil.copy(os.path.join(src_dir,'_test.so'), build_dir)
-			os.remove(os.path.join(src_dir,'test.o',))
-			os.remove(os.path.join(src_dir,'_test.so',))
+			#~ shutil.copy(os.path.join(src_dir,'_test.so'), build_dir)
+			#~ os.remove(os.path.join(src_dir,'test.o',))
+			#~ os.remove(os.path.join(src_dir,'_test.so',))
 		self.execute(compile, [], 'compiling Test')
 		os.chdir( starting_dir )
 		build.run(self)
 		
-short_description = 'A python extension for interfacing with the NXP PN512 NFC Reader. Targeted specifically for Raspberry Pi and the EXPLORE-NFC module'
+short_description = 'A python Interface for interfacing with the Raspberry Pi and the EXPLORE-NFC module'
 
-setup (name = 'nxppy',
-	   version = '1.2',
+setup (name = 'pynfc_nfx',
+	   version = '1.0',
 	   description = short_description, 
 	   #~ long_description = long_description,
-	   author = 'Scott Vitale',
-	   #~ author_email = 'svvitale@gmail.com',
-	   #~ url = 'http://github.com/svvitale/nxppy',
-	   #~ test_suite = 'nose.collector',
-	   #~ setup_requires=['nose>=1.0'],
-	   #~ ext_modules = [nxppy],
-	   cmdclass = {'build': build_nxppy})
+	   author = 'Timothy C Eichler',
+	   author_email = 'timeyyy_da_man@hotmail.com',
+	   url = 'https://github.com/timeyyy/pynfc_nfx',
+	   cmdclass = {'build': build_pynfc_nfx})
 
